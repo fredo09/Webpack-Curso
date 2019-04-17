26f8c8b2839c16604af99c3b8bcccffc798dbc7a
Fredy <desaMts1806@MacBook-Pro-de-macpro.local>
2019-04-17 01:43:44 -0500

    Agregando soporte para archivos json con json-loader webpack

---
+++ json-loader/src/js/index.js
@@ -0,0 +1,29 @@
+import './../css/estilos.css';
+import Img_logo from './../img/platzi.png';
+import Data from './prof.json';
+const { Message, el_mss } = require('./message');
+const {ToDom} = require('./reder-to-dom');
+
+document.write(Message);
+
+console.log(Data.profes);
+
+Data.profes.forEach( (profes) => {
+    const lista = document.createElement('li');
+    lista.textContent = profes.name;
+    ToDom(lista);
+});
+
+const html_img = document.createElement('img');
+
+html_img.setAttribute('src' ,Img_logo );
+console.log(html_img);
+html_img.setAttribute('width', 50);
+html_img.setAttribute('height', 50);
+
+document.body.append(html_img);
+
+
+
+el_mss();
+console.log("Hola Mundo, Webpack V0.2 external BABEL_JSON" );
