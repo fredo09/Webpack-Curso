dd563b3ddd3037f53e5e92ee0411d4b4465a494b
Fredy <desaMts1806@MacBook-Pro-de-macpro.local>
2019-04-17 00:39:58 -0500

    Agregando soporte para funtes con webpack ahora la estructura de carpetas esta mas ordenado

---
+++ url-loader-fonts/src/js/index.js
@@ -0,0 +1,17 @@
+import './../css/estilos.css';
+import Img_logo from './../img/platzi.png';
+const { Message, el_mss } = require('./message');
+
+document.write(Message);
+
+const html_img = document.createElement('img');
+
+html_img.setAttribute('src' ,Img_logo );
+console.log(html_img);
+html_img.setAttribute('width', 50);
+html_img.setAttribute('height', 50);
+
+document.body.append(html_img);
+
+el_mss();
+console.log("Hola Mundo, Webpack V0.2 external BABEL" );
---
+++ url-loader-fonts/webpack.config.js
@@ -0,0 +1,143 @@
+/** 
+ *  Configuracion de Webpack  loader
+ */
+
+ //imports
+ const path = require('path'); //Rutas relativas para los archivos
+ const ExtractTextPlugin = require('extract-text-webpack-plugin'); //Necesario para extraer texto a otros archivos css
+//Se exporta el modulo
+ module.exports={
+     entry: ['babel-polyfill',path.resolve(__dirname, 'src/js/index.js')], //Archivo de Entrada
+     output:{
+         path: path.resolve(__dirname, 'dist'),//Ruta en donde puedo dejar el archivo compilado
+         filename: "bundle_external.js" //Archivo de Salida
+     },
+     module:{
+         rules:[
+            { //Configutacion para agregar babel en webpack
+              test: /\.js$/,
+              exclude: /(node_modules|bower_components)/,
+              use: {
+                loader: 'babel-loader',
+                options: {
+                  presets: ['@babel/preset-env'],//Nueva version 
+                    //presents: ['es2015']
+                }
+              }
+            },
+            {  //loader para el reconocimiento de imagenes 
+               test: /\.(png|jpg|gif|woff|eot|ttf|svg)$/i,  //Se agrega soporte para las fuentes tambien
+               use : { //Requiere una configuracion especial
+                  loader: 'url-loader', // usando loader url
+                  options :{
+                     limit : 100000
+                  }
+               }
+            },
+            {
+                test:/\.css$/,
+               // use:['style-loader','css-loader']
+              use: ExtractTextPlugin.extract({ 
+                  use:'css-loader',
+               // fallback:'style-loader' puede o no ser neseario para extraer el archivo css
+                 })
+            }
+         ]
+     },
+     plugins:[ //Aqui van siempre los plugins
+       // new ExtractTextPlugin("css/styles.css") //lugar para poner el nuevo archivo eztraido 
+       new ExtractTextPlugin("css/[name].css")    
+     ]
+ }
+
+
+ /**
+  * Cargar loaders
+  * module:{
+  *     rules:[
+  *         { Aqui van los loaders
+  *             test: Archivos que quiero reconocer se aplica con expresiones regulares /\.css$/
+  *             use: Que loader se va a encargar del archivo
+  *             
+  *    
+                test:/\.css$/, //Expresion Regular para ver que archivos quiero reconocer
+                use:['style-loader','css-loader'] // style-loader-> inprime el codigo css , css-loader->lee codigo css en archivo js 
+  *         
+  *         }
+  *     ]
+  * }
+  * 
+  * 
+  */
+
+  ////////////////////////////////////////////////////////////////////////////////////////
+  /**
+   * //Plugins
+   * module :{
+   *    rules:[
+   *    {
+   *        test: /\.css$/,
+   *        use: ExtractTextPlugin.estract({ use:'css-loader', // fallback:'style-loader' }) // el codigo css que se extraera
+   *    }
+   * ]
+   * },
+   * plugins:[ //aqui van los plugins
+   *        new ExtractTextPlugin("css/styles.css") //lugar para poner el nuevo archivo eztraido
+   *        new ExtractTextPlugin("css/[name].css") // [name] -> me sirve para poner el nombre del entrypoint al archivo css 
+   *    ]
+   */
+
+   /////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    /**
+     *  Loaders para otros archivos de Javascript Ejemplo BABEL.JS
+     * 
+     *  module:{
+     *      rules:[
+     *          {
+     *              test: /\.js$/,
+     *              use:{ 
+     *                      loader: 'babel-loader'  //Forma de hacer un loader con configuracion especial
+     *                      options: {  //Nos sirve para ver que version de ecmaScript se va a usar
+     *                                  presets: ['es2015', 'es2016'] 
+     *                               }
+     *                   }
+     *          }
+     *      ]
+     *  }
+     * 
+     * ---
+     *  {  // Agrgando un nuevo loader
+                test:/\.js$/,
+                use:{ // Usando loader para incorporar babel  con una configuracion especial
+                    loader: 'babel-loader',
+                    options:{
+                     presets:['es2015'] //Version de ecmaScript a usar
+                     //presents : ['babel-preset-es2015']
+                    // presents: ['@babel/preset-env']
+                    }    
+                }
+            },
+     */
+
+/////////////////////////////////////////////////////////////////////////////
+
+//URL-LOADER
+
+/**
+ *  No es necesario declarar los loaders para usarlos 
+ *  module:{ // Aqui van los loader
+ *      rules:[
+ *          {
+ *              test : /\.(jpg|jpeg|png|gif|icon|woff|eot|ttf|svg)$/, //Extenciones a reconocer ademas de agregar soporte para las fuentes
+ *              use : {  // Configuracion especial
+ *                      loader : 'url-loader',
+ *                      options : {
+ *                        limit : 100000
+ *                      }
+ *              }
+ *          }  
+ *    ]
+ * }
+ * 
+ */
